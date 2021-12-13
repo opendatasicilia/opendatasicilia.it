b846c1b7fa0bbe11c70bdbc018391907959a5f8b
Gabriele Scalici <gaba41@gmail.com>
2021-12-13 09:23:00 +0100

    gh-pages no prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 require("dotenv").config({path: `.env.${process.env.NODE_ENV}`})
   
 module.exports = {
-  pathPrefix: '/opendatasicilia.it',
   plugins: [
     {
       resolve: "gatsby-source-wordpress",
