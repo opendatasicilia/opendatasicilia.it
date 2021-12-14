6705926c838a9d8e068affbcefbd9ab9ca65c310
Gabriele Scalici <gaba41@gmail.com>
2021-12-14 10:34:29 +0100

    404 prototype

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Select from 'react-select'
+
+export default function Search({data}){
+    const posts = data.allWpPost.nodes
+    const options = posts.map(post => (
+        {
+            value: post.slug,
+            label: post.title
+        }
+    ))
+    const handleChange = (selectedOption) => {
+        if (typeof window !== 'undefined') {
+            window.location = `../blog/${selectedOption.value}`;
+          }
+    };
+    return(
+        <>
+        <Select options={options} onChange={handleChange} />
+        </>
+    )
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,41 @@
-import * as React from "react"
+import React from "react"
+import Layout from "../components/Layout"
+import Search from "../components/Search"
+import { graphql } from "gatsby"
 
-export default function NotFound(){
-
-  React.useEffect(() => {
-    let interval;
-       interval = setInterval(() => {
-        if (typeof window !== 'undefined') {
-            window.location = `/`;
-          }
-       }, 1);
-    return () => interval ? clearInterval(interval) : null;
-  }, [])
+export default function NotFound({data}){
 
   return(
-      <div/>
+      <Layout title="404">
+      <div className="container">
+        <div className="row pt-5">
+          <div className="col-12 col-lg-6 align-self-center">
+            <h1>
+              Pagina non trovata
+            </h1>
+            <h2 className="fw-light">
+              Siamo spiacenti, questa pagina non è stata trovata o non esiste. Vuoi provare a cercarla?
+            </h2>
+            <Search data={data}/>
+          </div>
+          <div className="col-12 col-lg-6">
+            <h1 style={{color:'#00A47F', fontSize:'281px', textShadow:'6px 0px 0px #00000029', fontWeight:'900'}}>
+              404
+            </h1>
+          </div>
+        </div>
+      </div>
+      </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allWpPost {
+      nodes {
+        title
+        slug
+      }
+    }
+  }
+`
