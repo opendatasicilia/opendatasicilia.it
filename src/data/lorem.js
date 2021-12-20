2f830b63cf0854157ce384017ca7f05618a6d280
Gabriele Scalici <gaba41@gmail.com>
2021-12-20 14:12:23 +0100

    test cache speed #62

--- src/data/lorem.js
+++ src/data/lorem.js
@@ -1,2 +1,2 @@
-const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales tincidunt risus vel tempus. Nunc lobortis faucibus enim in iaculis. Nulla urna metus, gravida faucibus varius eu, imperdiet et leo. Proin finibus, lectus convallis consequat posuere, quam ex rhoncus lectus, consequat hendrerit odio velit nec purus."
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales tincidunt risus vel tempus. Nunc lobortis faucibus enim in iaculis. Nulla urna metus, gravida faucibus varius eu, imperdiet et leo. Proin finibus, lectus convallis consequat posuere, quam ex rhoncus lectus, consequat hendrerit odio velit nec purus. "
 export default lorem
