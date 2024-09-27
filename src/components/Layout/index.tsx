c288f71c95ed1caec773558a8983ae54f200e768
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2024-09-28 00:28:19 +0200

    fix: use cdn for masonry

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -19,6 +19,12 @@ export const Layout = ({ children, title }: LayoutProps) => (
       <title>
         {title ? title + " | Open Data Sicilia" : "Open Data Sicilia"}
       </title>
+      <script
+        src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+        integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D"
+        crossOrigin="anonymous"
+        async
+      ></script>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         rel="preconnect"
