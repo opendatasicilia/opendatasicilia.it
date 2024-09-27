f95666574fd428930f5b192318a087927bfc3de0
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2024-09-28 00:51:29 +0200

    Revert "fix: use cdn for masonry"
    This reverts commit c288f71c95ed1caec773558a8983ae54f200e768.

--- src/components/Artifacts/index.tsx
+++ src/components/Artifacts/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Masonry from "masonry-layout";
 
 import { ArtifactType } from "@types";
 import { Artifact } from "./partials/Artifact";
@@ -6,15 +7,29 @@ import { Artifact } from "./partials/Artifact";
 type ArtifactProps = {
   artifacts: ArtifactType[];
 };
+export const Artifacts = ({ artifacts }: ArtifactProps) => {
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+    const grid = document.querySelector(".row");
+    if (grid) {
+      new Masonry(grid, {
+        percentPosition: true,
+        itemSelector: ".col-lg-4",
+      });
+    }
+  }, [artifacts]);
 
-export const Artifacts = ({ artifacts }: ArtifactProps) => (
-  <div className="row" data-masonry='{"percentPosition": true }'>
-    {artifacts.map((artifact, index) => (
-      <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index}>
-        <div className="card p-3">
-          <Artifact artifact={artifact} />
+  return (
+    <div className="row">
+      {artifacts.map((artifact, index) => (
+        <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index}>
+          <div className="card p-3">
+            <Artifact artifact={artifact} />
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
