ef860fca6998b5db42fccb7e6c9b5428f743dc11
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2024-09-28 11:53:00 +0200

    fix: masonry hydrate on refresh

--- src/components/Artifacts/index.tsx
+++ src/components/Artifacts/index.tsx
@@ -1,20 +1,44 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import { ArtifactType } from "@types";
 import { Artifact } from "./partials/Artifact";
+import { IS_BROWSER } from "@utils/helpers";
 
 type ArtifactProps = {
   artifacts: ArtifactType[];
 };
 
-export const Artifacts = ({ artifacts }: ArtifactProps) => (
-  <div className="row" data-masonry='{"percentPosition": true }'>
-    {artifacts.map((artifact, index) => (
-      <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index}>
-        <div className="card p-3">
-          <Artifact artifact={artifact} />
+export const Artifacts = ({ artifacts }: ArtifactProps) => {
+  const [Masonry, setMasonry] = useState<any>(null);
+
+  useEffect(() => {
+    if (IS_BROWSER) {
+      import("masonry-layout").then((MasonryModule) => {
+        setMasonry(() => MasonryModule.default || MasonryModule);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Masonry && IS_BROWSER) {
+      const grid = document.querySelector(".row");
+      if (grid) {
+        new Masonry(grid, {
+          itemSelector: ".col-lg-4",
+          percentPosition: true,
+        });
+      }
+    }
+  }, [Masonry, artifacts]);
+
+  return (
+    <div className="row">
+      {artifacts.map((artifact, index) => (
+        <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index}>
+          <div className="card p-3">
+            <Artifact artifact={artifact} />
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
