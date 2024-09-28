05f315af45a4d9c64f86d88b668e24e42f6a8212
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2024-09-28 12:16:01 +0200

    style: card dimensions

--- src/components/Artifacts/partials/Artifact/index.tsx
+++ src/components/Artifacts/partials/Artifact/index.tsx
@@ -9,31 +9,29 @@ export const Artifact = ({ artifact }: { artifact: ArtifactType }) => {
   const href = `/${artifact.type}/${artifact.name}`;
 
   return (
-    <>
-      <div className="row mb-3">
-        <div className="col-auto pe-0">
-          <ProjectIcon type={artifact.type} />
-        </div>
-        <div className="col">
-          <h2 className="h3 fw-bold m-0">{artifact.title}</h2>
-          <div className="d-flex mt-2">
-            <ProjectType type={artifact.type} />
-            {artifact.created && (
-              <DateComponent date={artifact.created} type="created" />
-            )}
-            {artifact.edited && (
-              <DateComponent date={artifact.edited} type="edited" />
-            )}
-          </div>
-          <div className="mt-3">
-            <p>{artifact.description}</p>
-          </div>
-          <div>
-            <Link to={href}>Dettagli</Link>
-          </div>
-          {/* <pre>{JSON.stringify(artifact, null, 2)}</pre> */}
-        </div>
+    <div className="row mb-3">
+      <div className="col-auto pe-0">
+        <ProjectIcon type={artifact.type} />
       </div>
-    </>
+      <div className="col">
+        <h2 className="h4 fw-bold m-0">{artifact.title}</h2>
+        <div className="d-flex mt-2">
+          <ProjectType type={artifact.type} />
+          {artifact.created && (
+            <DateComponent date={artifact.created} type="created" />
+          )}
+          {artifact.edited && (
+            <DateComponent date={artifact.edited} type="edited" />
+          )}
+        </div>
+        <div className="mt-3">
+          <p>{artifact.description}</p>
+        </div>
+        <div>
+          <Link to={href}>Dettagli</Link>
+        </div>
+        {/* <pre>{JSON.stringify(artifact, null, 2)}</pre> */}
+      </div>
+    </div>
   );
 };
